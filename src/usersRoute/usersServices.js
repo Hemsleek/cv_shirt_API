00a0799b118214f339e3b9d842cdce3999cb1901
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-09-02 12:56:41 +0100

    fix: return type

--- src/usersRoute/usersServices.js
+++ src/usersRoute/usersServices.js
@@ -4,65 +4,64 @@
 import Users from './UsersModel';
 
 export const login = async (req, res) => {
-    const excludeFromResult = '-createdAt -updatedAt -__v -password';
+  const excludeFromResult = '-createdAt -updatedAt -__v -password';
 
-    try {
-        console.log('hereeee');
-        const { email } = req.body;
-        console.log(' two hereeee');
+  try {
+    console.log('hereeee');
+    const { email } = req.body;
+    console.log(' two hereeee');
 
-        const userExist = await Users.findOne({ email }, excludeFromResult);
-        console.log({ userExist });
-        if (!userExist) {
-            return res.status(404).json({
-                message: 'incorrect credentials',
+    const userExist = await Users.findOne({ email }, excludeFromResult);
+    console.log({ userExist });
+    if (!userExist) {
+      return res.status(404).json({
+        message: 'incorrect credentials',
 
-            });
-        }
-        return res.json({
-            message: 'login successful',
-            payload: userExist,
-        });
-    } catch (err) {
-        return res.status(500).send('internal server error');
+      });
     }
+    return res.json({
+      message: 'login successful',
+      payload: {
+        email: userExist.email,
+        fullName: userExist.fullname,
+      },
+    });
+  } catch (err) {
+    return res.status(500).send('internal server error');
+  }
 };
 
 export const getAllUsers = async (req, res) => {
-    const excludeFromResult = '-createdAt -updatedAt -__v';
+  const excludeFromResult = '-createdAt -updatedAt -__v';
 
-    try {
-        const allUsers = await Users.find({}, excludeFromResult);
-        return res.json(allUsers);
-    } catch (err) {
-        return res.status(500).send('internal server error');
-    }
+  try {
+    const allUsers = await Users.find({}, excludeFromResult);
+    return res.json(allUsers);
+  } catch (err) {
+    return res.status(500).send('internal server error');
+  }
 };
 
 export const createUser = async (req, res) => {
-    const excludeFromResult = '-createdAt -updatedAt -__v -password';
+  const excludeFromResult = '-createdAt -updatedAt -__v -password';
 
-    try {
-        const { email, password, fullname } = req.body;
-        const userExist = await Users.findOne({ email }, excludeFromResult);
+  try {
+    const { email, password, fullname } = req.body;
+    const userExist = await Users.findOne({ email }, excludeFromResult);
 
-        if (userExist) {
-            return res.status(409).json({
-                message: 'user already exist, login instead',
+    if (userExist) {
+      return res.status(409).json({
+        message: 'user already exist, login instead',
 
-            });
-        }
-        const newUser = await new Users({ email, password, fullname }).save();
-
-        return res.json({
-            message: 'operation successful, proceed to login',
-            payload: {
-                fullname: newUser.fullname,
-                email: newUser.email,
-            },
-
-        });
-    } catch (err) {
-        return res.status(500).send('internal server error');
+      });
     }
+    await new Users({ email, password, fullname }).save();
+
+    return res.json({
+      message: 'operation successful, proceed to login',
+
+    });
+  } catch (err) {
+    return res.status(500).send('internal server error');
+  }
 };
